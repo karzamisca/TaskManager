6080d592edf10662d6a85b2c38d06dc30a82a136
misca <miscakarza@gmail.com>
2024-10-16 15:30:58 +0700

    authentication

--- routes/entryRoutes.js
+++ routes/entryRoutes.js
@@ -8,7 +8,7 @@ const router = express.Router();
 // Configure multer for file uploads
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "uploads/"); // Upload files to the 'uploads' directory
+    cb(null, "public/uploads/"); // Upload files to the 'uploads' directory
   },
   filename: function (req, file, cb) {
     // Use the original name
@@ -37,12 +37,13 @@ const upload = multer({ storage: storage, fileFilter: fileFilter });
 router.get("/entry", authMiddleware, entryController.getFormAndEntries); // Serve HTML form and table
 router.get("/entryAll", authMiddleware, entryController.getAllEntries); // Serve all entries as JSON
 router.post("/entryNew", authMiddleware, entryController.createEntry); // Submit new entry
-router.delete("/entryDelete/:id", entryController.deleteEntry); // Delete entry by ID
+router.delete("/entryDelete/:id", authMiddleware, entryController.deleteEntry); // Delete entry by ID
 
 // Export and Import routes
-router.get("/entryExport", entryController.exportToExcel); // Export entries to Excel
+router.get("/entryExport", authMiddleware, entryController.exportToExcel); // Export entries to Excel
 router.post(
   "/entryImport",
+  authMiddleware,
   upload.single("excelFile"),
   entryController.importFromExcel
 ); // Import entries from Excel
