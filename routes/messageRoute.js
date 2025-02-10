ac68676a2661e0bc879a29ae5713846763b99c65
miscakarza <miscakarza@gmail.com>
2025-02-11 03:51:04 +0700

    add private room for message board

--- routes/messageRoute.js
+++ routes/messageRoute.js
@@ -16,4 +16,25 @@ router.post("/messagePost", authMiddleware, messageController.postMessage);
 // Public route to get messages
 router.get("/messageGet", authMiddleware, messageController.getMessages);
 
+router.post("/room/create", authMiddleware, messageController.createRoom);
+router.get("/rooms", authMiddleware, messageController.getRooms);
+router.post("/room/message", authMiddleware, messageController.postRoomMessage);
+router.get(
+  "/room/:roomId/messages",
+  authMiddleware,
+  messageController.getRoomMessages
+);
+router.get("/users", authMiddleware, messageController.getUsers);
+router.post(
+  "/room/members/add",
+  authMiddleware,
+  messageController.addMembersToRoom
+);
+router.post(
+  "/room/members/remove",
+  authMiddleware,
+  messageController.removeMemberFromRoom
+);
+router.delete("/room/:roomId", authMiddleware, messageController.deleteRoom);
+
 module.exports = router;
