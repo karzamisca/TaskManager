79eefc8e9a5bfc817f757c7f1972c8914bae1672
karzamisca <miscakarza@gmail.com>
2025-05-27 17:48:14 +0700

    change role access in user salary record

--- controllers/userController.js
+++ controllers/userController.js
@@ -85,7 +85,7 @@ exports.getManagers = async (req, res) => {
 // Get all users except privileged roles
 exports.getAllUsers = async (req, res) => {
   try {
-    const privilegedRoles = ["superAdmin", "deputyDirector", "director"];
+    const privilegedRoles = ["superAdmin", "director"];
 
     // Always exclude privileged roles from results
     const baseQuery = {
